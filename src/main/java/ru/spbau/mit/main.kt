9c04389850a46dd5ea2037e66b9e6699d4ba6c08
Yurii Rebryk <y.a.rebryk@gmail.com>
2017-10-27 17:46:53 +0300

    Implement task-1

--- src/main/java/ru/spbau/mit/main.kt
+++ src/main/java/ru/spbau/mit/main.kt
@@ -1,14 +1,110 @@
 package ru.spbau.mit
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    
-    words.add("world!")
+import java.io.InputStreamReader
+import java.util.*
 
-    return words.joinToString(separator = " ")
+
+class BinarySegmentTree(private val size: Int) {
+    private val elements: Array<Int> = Array(4 * size) { 0 }
+
+    fun set(index: Int, value: Boolean) = set(1, 0, size, index, value)
+
+    fun get(index: Int) = get(1, 0, size, index)
+
+    fun getKth(k: Int): Int = getKth(1, 0, size, k)
+
+    private fun getKth(node: Int, left: Int, right: Int, k: Int): Int {
+        if (left == right) {
+            return left
+        }
+
+        val middle = getMiddle(left, right)
+
+        return if (k <= elements[leftNode(node)]) {
+            getKth(leftNode(node), left, middle, k)
+        } else {
+            getKth(rightNode(node), middle + 1, right, k - elements[leftNode(node)])
+        }
+    }
+
+    private fun set(node: Int, left: Int, right: Int, index: Int, value: Boolean): Unit {
+        if (left == right) {
+            elements[node] = if (value) 1 else 0
+        } else {
+            val middle = getMiddle(left, right)
+
+            if (index <= middle) {
+                set(leftNode(node), left, middle, index, value)
+            } else {
+                set(rightNode(node), middle + 1, right, index, value)
+            }
+
+            elements[node] = elements[leftNode(node)] + elements[rightNode(node)]
+        }
+    }
+
+    private fun get(node: Int, left: Int, right: Int, index: Int): Boolean {
+        if (left == right) {
+            return elements[node] == 1
+        }
+
+        val middle = getMiddle(left, right)
+
+        return if (index <= middle) {
+            get(leftNode(node), left, middle, index)
+        } else {
+            get(rightNode(node), middle + 1, right, index)
+        }
+    }
+
+    private fun leftNode(node: Int) = 2 * node
+
+    private fun rightNode(node: Int) = 2 * node + 1
+
+    private fun getMiddle(left: Int, right: Int): Int = left + (right - left) / 2
 }
 
-fun main(args: Array<String>) {
-    println(getGreeting())
+interface Problem<out T> {
+    fun solve(): T
 }
+
+class StringManipulation(private val name: String,
+                         private val queries: List<Query>) : Problem<String> {
+    private val trees: Array<BinarySegmentTree> = Array(LETTERS_COUNT) {
+        BinarySegmentTree(name.length)
+    }
+
+    override fun solve(): String {
+        name.forEachIndexed { index, character -> getTreeByCharacter(character).set(index, true) }
+
+        queries.forEach {
+            val tree = getTreeByCharacter(it.character)
+            tree.set(tree.getKth(it.index), false)
+        }
+
+        return name.filterIndexed { index, character -> getTreeByCharacter(character).get(index) }
+    }
+
+    private fun getTreeByCharacter(char: Char): BinarySegmentTree = trees[char - 'a']
+
+    companion object {
+        val LETTERS_COUNT: Int = 26
+
+        fun read(scanner: Scanner): StringManipulation {
+            val repetitionCount = scanner.nextInt()
+            scanner.nextLine()
+            val pattern = scanner.nextLine()
+            val queries = (1..scanner.nextInt()).map { Query.read(scanner) }
+
+            return StringManipulation(pattern.repeat(repetitionCount), queries)
+        }
+    }
+
+    data class Query(val index: Int, val character: Char) {
+        companion object {
+            fun read(scanner: Scanner) = Query(scanner.nextInt(), scanner.nextLine()[1])
+        }
+    }
+}
+
+fun main(args: Array<String>) = println(StringManipulation.read(Scanner(InputStreamReader(System.`in`))).solve())
--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,51 @@
 package ru.spbau.mit
-import kotlin.test.assertEquals
+
 import org.junit.Test
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testEmptyBinarySegmentTree() {
+        val tree = BinarySegmentTree(5)
+
+        assertEquals(false, tree.get(0), "Method `get` fails")
+        assertEquals(0, tree.getKth(0), "Method `getKth` fails")
+        assertEquals(5, tree.getKth(1), "Method `getKth` fails")
+    }
+
+    @Test
+    fun testBinarySegmentTree() {
+        val tree = BinarySegmentTree(5)
+
+        val ones = (0 until 5 step 2)
+        ones.forEach { tree.set(it, true) }
+        ones.forEach {
+            assertEquals(true, tree.get(it), "Method `get` or `set` fails")
+        }
+
+        val zeros = (1 until 5 step 2)
+        zeros.forEach {
+            assertEquals(false, tree.get(it), "Method `get` or `set` fails")
+        }
+
+        assertEquals(0, tree.getKth(0), "Method `getKth` fails")
+        assertEquals(4, tree.getKth(3), "Method `getKth` fails")
+        assertEquals(5, tree.getKth(4), "Method `getKth` fails")
+    }
+
+    @Test
+    fun testStringManipulationExample1() {
+        assertTrue(testExample("2\nbac\n3\n2 a\n1 b\n2 c", "acb"), "Wrong answer on the first example")
+    }
+
+    @Test
+    fun testStringManipulationExample2() {
+        assertTrue(testExample("1\nabacaba\n4\n1 a\n1 a\n1 c\n2 b", "baa"), "Wrong answer on the second example")
+    }
+
+    private fun testExample(input: String, answer: String): Boolean {
+        return StringManipulation.read(Scanner(input.reader())).solve() == answer
     }
 }
